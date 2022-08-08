53751c2fed8763a36d262cefbf38f817f61082d7
aniasthana <99976702+aniasthana@users.noreply.github.com>
2022-08-08 19:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+video = "";
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480,300);
+    canvas.center();
+}
+function draw(){
+    image(video, 0, 0, 480, 300);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    video.hide();
+    video.speed(1);
+    video.volume(0);
+}
